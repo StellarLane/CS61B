184b6f27c93f35cf2e6132aa738ba7b94815c30f
StellarLane <stellarlane2@gmail.com>
2024-08-10 23:31:36 +0800

    a small fix for lab7 and implemented the optional remove function

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -5,10 +5,10 @@ import java.util.Set;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
     private class Node {
-        public K key;
-        public V value;
-        public Node leftChild;
-        public Node rightChild;
+        private K key;
+        private V value;
+        private Node leftChild;
+        private Node rightChild;
 
         public Node(K thisKey, V thisValue) {
             key = thisKey;
@@ -112,16 +112,70 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
 
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        if (containsKey(key)) {
+            V tmp = get(key);
+            root = nodeRemove(root, key);
+            size--;
+            return tmp;
+        } else {
+            return null;
+        }
+//        throw new UnsupportedOperationException();
+    }
+
+    private Node nodeRemoveHelper(Node node) {
+        if (node.leftChild == null) {
+            return node;
+        } else {
+            return nodeRemoveHelper(node.leftChild);
+        }
+    }
+
+    private Node nodeRemove(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp != 0) {
+            if (cmp < 0) {
+                node.leftChild = nodeRemove(node.leftChild, key);
+            } else {
+                node.rightChild = nodeRemove(node.rightChild, key);
+            }
+        } else {
+            Node removedNode = node;
+            if (removedNode.leftChild == null) {
+                return node.rightChild;
+            }
+            if (removedNode.rightChild == null) {
+                return node.leftChild;
+            }
+            node = nodeRemoveHelper(node.rightChild);
+            node.leftChild = removedNode.leftChild;
+            node.rightChild = nodeRemove(node.rightChild, key);
+        }
+        return node;
     }
 
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (containsKey(key) && get(key) == value) {
+            V tmp = get(key);
+            nodeRemove(root, key);
+            size--;
+            return tmp;
+        } else {
+            return null;
+        }
+//        throw new UnsupportedOperationException();
     }
 
     @Override
     public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void printInOrder() {
         throw new UnsupportedOperationException();
     }
 }
